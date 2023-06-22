3e39f4cecf2107db74b06973f657156c9c04f136
Stephen Gurett <stephen.gurnett@gmail.com>
2023-06-22 22:51:43 +0200

    Update minimum iOS version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "SDWebImage",
     platforms: [
         .macOS(.v10_11),
-        .iOS(.v9),
+        .iOS(.v13),
         .tvOS(.v9),
         .watchOS(.v2)
     ],
